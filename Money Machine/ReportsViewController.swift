9824cc94e6ea232f18c0b97bdceb42799d25ae68
robfaiella <tzr@mac.com>
2019-03-09 18:34:41 -0500

    naviation to screens

---
+++ Money Machine/ReportsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ReportsViewController.swift
+//  Money Machine
+//
+//  Created by Rob Faiella on 3/9/19.
+//  Copyright © 2019 robfaiella. All rights reserved.
+//
+
+import UIKit
+
+class ReportsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    
+    // Actions
+    
+ 
+    
+    
+    
+    // Segue
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+     
+    }
+
+
+}
+
---
+++ Money Machine/SearchViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SearchViewController.swift
+//  Money Machine
+//
+//  Created by Rob Faiella on 3/9/19.
+//  Copyright © 2019 robfaiella. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    
+    // Actions
+    
+ 
+    
+    
+    
+    // Segue
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+     
+    }
+
+
+}
+
